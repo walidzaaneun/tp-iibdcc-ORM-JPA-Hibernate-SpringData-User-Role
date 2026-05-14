720d65cd4d525d86ccbddf175e2cf25413a94290
walidzaaneun <zaanoun3@gmail.com>
2026-05-14 14:12:56 +0100

    first commit

---
+++ src/main/java/com/walidzaaneun/tpiibdccspringdatauserrole/TpIibdccSpringDataUserRoleApplication.java
@@ -0,0 +1,13 @@
+package com.walidzaaneun.tpiibdccspringdatauserrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIibdccSpringDataUserRoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpIibdccSpringDataUserRoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walidzaaneun/tpiibdccspringdatauserrole/TpIibdccSpringDataUserRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walidzaaneun.tpiibdccspringdatauserrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIibdccSpringDataUserRoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
